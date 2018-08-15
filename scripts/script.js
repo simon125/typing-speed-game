b7c15e5b1e3d8a34e78d9af4cad482d48a26bf91
Szymon Oleszek <szymqwerty@gmail.com>
2018-08-15 14:10:47 +0200

    function init added with most funcion

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+let time = 5
+let score = 0
+let isPlaying // true - false
+
+const wordInput = document.querySelector('#word-input')
+const currentWord = document.querySelector('#current-word')
+const scoreDisplay = document.querySelector('#score')
+const timeDisplay = document.querySelector('#time')
+const message = document.querySelector('#message')
+const seconds = document.querySelector('#seconds')
+
+const words = [
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+]
+
+const showWord = () => {
+    const randIndex = Math.floor(Math.random() * words.length)
+    currentWord.innerHTML = words[randIndex]
+}
+
+const init = () => {
+    showWord(words)
+    wordInput.addEventListener('input', startMatch)
+    setInterval(countDown, 1000)
+    setInterval(checkGameStatus, 50)
+}
